eae971222f2604e1b56f2dd4834a0a78b1ff09a6
Ridge Taylor <113702161+13yrsold@users.noreply.github.com>
2024-12-13 09:03:14 -0500

    Create yes.cpp

---
+++ yes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+std::string function{ "int" };
+
+function getKing() {
+    int bit32{ 86 };
+
+    return bit32;
+};
+
+int main {
+    std::cout << getKing;
+    return 1
+}
